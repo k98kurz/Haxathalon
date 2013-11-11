7fd07d6303984875cd40909ce9c434ba46e8aae2
Jonathan Voss <k98kurz@gmail.com>
2013-11-11 15:04:56 -0500

    Initial Commit
    Everything I've thought of thus far.

---
+++ Games/Algorithm Examples/2D Non-Vectored Damage Calculator/Algorithm Snippet.js
@@ -0,0 +1,85 @@
+function server () {
+	
+	this.queue = new function () {
+		this.fire = new function () {
+			this.list = [];
+			this.add = function (f) {
+				this.list.push(f); // f = [name, x, y]
+			};
+			this.clear = function () { this.list = []; }
+		};
+		this.evade = new function () {
+			this.list = [];
+			this.add = function (v) {
+				this.list.push(v); // v = [name, x, y]
+			};
+		};
+	};
+	
+	// loops through fire commands and compares to evade commands of targets
+	this.tick = function () {
+		for (i=0;i<this.queue.fire.list.length;i++) {
+			ev = this.queue.evade.find(this.queue.fire.list[i][0]);
+			this.compare(this.queue.fire.list[i], ev);
+		}
+		this.update();
+	};
+	
+	// x and y coords from fire and evade compared
+	// damage = maxdamage - (x+y)*damagedifference
+	this.compare = function (fire, evade) {
+		var x, y, d;
+		x = ((fire[1] - evade[1])>0) ? fire[1] - evade[1] : evade[1] - fire[1];
+		y = ((fire[2] - evade[2])>0) ? fire[2] - evade[2] : evade[2] - fire[2];
+		d = server.fireDamageMax - (x+y)*server.fireDamageDiff;
+		if (d>0) { this.clients.assignDamage(evade[0],d); }
+	};
+	
+	this.clients = new function () {
+		clients = [];
+		sockets = [];
+		
+		this.add = function (name, socket) {
+			function client () {
+				var health = server.clientHealth;
+				this.assignDamage = function (d) {
+					health -= d;
+				};
+			}
+			clients.push([name, new client]);
+			sockets.push([name,socket]);
+		};
+		this.remove = function (name) {
+			for (i=0;i<clients.length;i++) {
+				if (clients[i][0]==name) {
+					clients.splice(i,1);
+					i = clients.length + 1;
+				}
+			}
+		};
+		this.assignDamage = function (name, damage) {
+			for (i=0;i<this.clients.length;i++) {
+				if (this.clients[i][0]==name) {
+					this.clients[i][1]-=damage;
+					if (this.clients[i][1]<0) { this.clients[i][1] = 0; }
+				}
+			}
+		};
+		this.broadcast = function(message) {
+			for (i=0;i<clients.length;i++) {
+				// psuedo-code placeholder
+				sockets[i][1].push (message);
+			}
+		};
+		// send health updates to clients
+		this.update = function () {
+			for (i=0;i<clients.length;i++) {
+				this.broadcast(clients[i][0] + ":" + clients[i][1]);
+			}
+		};
+	};
+}
+server.fireDamageMax = 5;
+server.fireDamageDiff = 2;
+server.maxCoordChange = 3;
+server.clientHealth = 12;
---
+++ README.md
@@ -0,0 +1,25 @@
+Haxathalon Project Stuff
+=====
+
+Current projects are related to games and the like.
+Prototype/example code is most likely to be mostly JS/HTML.
+
+##Projects/to-do
+```
+1. 2D damage calculation algorithm
+  a) Made for a simple multiplayer space battle game.
+  b) Can be used in 3D games if 3D vectors are not needed.
+  c) Current scope is only JS/HTML for demonstration purposes--will likely port to Java and C++ for real games.
+  d) Turn-based AI simulation
+2. 2D gravity/force simulator
+  a) 2D force vector subtraction algorithms will be useful for more accurate/complex 2D damage calculation algorithms
+  b) May be useful for games involving dynamic orbital mechanics
+3. 2D top-down crew/squad controls
+  a) Group movements
+  b) Individual selection and actions
+  c) Area-of-effect example mechanics
+4. Actual multiplayer 2D space battle strategy game for Java
+  a) 2D damage calculations
+  b) Simulated 3rd spacial dimension for projectile impact timing
+  c) Either RTS or turn-based network operations
+  d) Possible Android port
